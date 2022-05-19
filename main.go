9498243d35dfd100023c3e59d3cba35d90d14291
roma_marshall <roma_marshall@outlook.com>
2022-05-19 10:17:33 +0200

    code refactoring

--- main.go
+++ main.go
@@ -8,15 +8,7 @@ import (
 func main() {
 	fmt.Println("enter matrix")
 
-	var a1 int
-	var a2 int
-	var a3 int
-	var b1 int
-	var b2 int
-	var b3 int
-	var c1 int
-	var c2 int
-	var c3 int
+	var a1, a2, a3, b1, b2, b3, c1, c2, c3 int
 
 	fmt.Fscan(os.Stdin, &a1, &a2, &a3)
 	fmt.Fscan(os.Stdin, &b1, &b2, &b3)
